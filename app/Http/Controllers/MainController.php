96d5e8b386fae0171133787e8029984e45386d18
Begbutaev Serik <begbutaevserik@gmail.com>
2022-04-27 11:47:04 +0500

    cleaned unused code

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\AppealRequest;
 use App\Models\Appeal;
 use Carbon\Carbon;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use function PHPUnit\Framework\isEmpty;
 
--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-
 Auth::routes([
     'reset' => false,
     'confirm' => false,
     'verify' => false,
 ]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/logout', [LoginController::class, 'logout'])->name('get-logout');
 
